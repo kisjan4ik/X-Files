8fb7d6844ed73f1879aa612e91c3cc6d219abac0
EpicGibbon <michant1997@gmail.com>
2020-07-23 12:48:30 -0400

    have initial map populating

---
+++ public/js/googlemapapi.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8
+    });
+}
