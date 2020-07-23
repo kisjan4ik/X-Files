2f5dac0ab7eb68931afd6f5dc131274a6a531240
Cameron Graham <cameron.g882@gmail.com>
2020-07-23 14:02:20 -0400

    fixed starting coords on map to center on US

--- public/js/googlemapapi.js
+++ public/js/googlemapapi.js
@@ -5,9 +5,9 @@ let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 39.1299577,
+            lng: -96.44058
         },
-        zoom: 8
+        zoom: 4.4
     });
 }
