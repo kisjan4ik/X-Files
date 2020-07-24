badbdcaf6f789deb8c19013194c60a0bc0600582
EpicGibbon <michant1997@gmail.com>
2020-07-24 10:46:14 -0400

    have added some additional functions for our api routes for functionality

---
+++ public/js/animation.js
@@ -0,0 +1,6 @@
+
+setTimeout(function() {
+  setInterval(function() {
+    $("#alien1").attr("src",$("#alien1").attr("src"));
+  },1);
+}, 4500);
--- public/js/googlemapapi.js
+++ public/js/googlemapapi.js
@@ -1,3 +1,5 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-use-before-define */
 "use strict";
 var locations = [];
 let map;
@@ -5,11 +7,11 @@ $.ajax({
   url: "/api/sighting",
   method: "GET"
 }).then(function (data) {
-  console.log(data)
+  console.log(data);
   for (let i = 0; i < data.length; i++) {
     var currentlocation = [];
     currentlocation = [`${data[i].latitude}`, data[i].longitude];
-    locations.push(currentlocation)
+    locations.push(currentlocation);
   } return locations;
   // console.log(locations);
 }).then(function (locations) {
@@ -18,7 +20,7 @@ $.ajax({
       lat: 39.1299577,
       lng: -96.44058
     },
-    zoom: 4.4
+    zoom: 4.4,
     styles: [
       {
         "elementType": "geometry",
@@ -359,22 +361,19 @@ $.ajax({
       }
     ]
   });
-}); console.log(locations);
+  console.log(locations);
+  for (i = 0; i < locations.length; i++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+      map: map
+    });
+    google.maps.event.addListener(marker, "click", (function (marker, i) {
+      return function () {
+        infowindow.setContent(locations[i][0]);
+        infowindow.open(map, marker);
+      };
+    })(marker, i));
+  }
+});
 
-for (i = 0; i < locations.length; i++) {
-
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(locations[i][0], locations[i][1]),
-    map: map
-  });
-  google.maps.event.addListener(marker, 'click', (function (marker, i) {
-    return function () {
-      infowindow.setContent(locations[i][0]);
-      infowindow.open(map, marker);
-    }
-  })(marker, i));
-}
-
-
-// var infowindow = new google.maps.InfoWindow();
-
+var marker, i;
