539c1a0a8281e15d51bf7da95a54833fc0cc7276
EpicGibbon <michant1997@gmail.com>
2020-07-27 11:19:15 -0400

    added some info to the array and cleaned up the folder structure

--- public/js/randomfacts.js
+++ public/js/randomfacts.js
@@ -11,6 +11,18 @@ const randomfacts = {
       fact: "62 out of 1,356 astronomers reported inexplicable ariel phenomena"
     }, {
       fact: "Alien sightings have been reported in many nuclear power plants"
+    },
+    {
+      fact: "We won’t see aliens until half of the Milky Way has been reached."
+    },
+    {
+      fact: "Scientists have sent aliens a mixtape."
+    }, {
+      fact: "40 billion Earth-like planets could exist in the Milky Way."
+    }, {
+      fact: "Area 51 has been around since 1955."
+    }, {
+      fact: "Alien life can be proved by gases in the atmosphere."
     }
   ],
   rimages: [
@@ -19,6 +31,18 @@ const randomfacts = {
     },
     {
       img: "https://media.gettyimages.com/illustrations/alien-artwork-illustration-id460713801?s=2048x2048"
+    }, {
+      img: "https://media.gettyimages.com/illustrations/illustration-of-a-humanoid-alien-illustration-id1124671867?s=2048x2048"
+    }, {
+      img: "https://www.history.com/.image/t_share/MTU3ODc3NjU1Mzk4ODUxOTEz/ufo-sightings-new-jersey-ufo-photos.jpg"
+    }, {
+      img: "https://media12.s-nbcnews.com/j/MSNBC/Components/Video/201909/ufothumb.focal-760x428.jpg"
+    },
+    {
+      img: "https://s.abcnews.com/images/International/abc_ann_china_ufo_100715_wg.jpg"
+    },
+    {
+      img: "https://www.gannett-cdn.com/-mm-/b2b05a4ab25f4fca0316459e1c7404c537a89702/c=0-0-1365-768/local/-/media/2019/04/05/USATODAY/usatsports/gettyimages-501124314.jpg?width=660&height=372&fit=crop&format=pjpg&auto=webp"
     }, {
       img: "https://media.gettyimages.com/illustrations/illustration-of-a-humanoid-alien-illustration-id1124671867?s=2048x2048"
     }, {
@@ -39,6 +63,18 @@ const randomfacts = {
       quote: "As human beings, we're born believing that we are the apex of creation, that we are invincible, that no problem exists that we cannot solve. But we inevitably die with all our beliefs broken. ― Shaun David Hutchinson, We Are the Ants"
     }, {
       quote: "It is surely unreasonable to credit that only one small star in the immensity of the universe is capable of developing and supporting intelligent life. But we shall not get to them and they will not come to us. ― P.D. James, The Children of Men"
+    },
+    {
+      quote: "The only thing that scares me more than space aliens is the idea that there aren't any space aliens. We can't be the best that creation has to offer. I pray we're not all there is. If so, we're in big trouble. ― Ellen DeGeneres"
+    },
+    {
+      quote: "Mankind will discover objects in space sent to us by the watchers...  ― Nostradamus"
+    }, {
+      quote: "Do you ever wonder at the arrogance of men who think they alone exist in the Universe? ― Nora Roberts, Stars of Fortune"
+    }, {
+      quote: "A species that is more intelligent and advanced than us will have nothing like a moral value system for sure."
+    }, {
+      quote: "God is playing out every possible scenario in an infinite number of universes all at the same time. We just happen to be stuck with this one… - Ska Martin, Aliens versus God"
     }
   ]
 
