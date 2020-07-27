c95ea52ab50f0b7f2944dd99b46551f02c0156c7
EpicGibbon <michant1997@gmail.com>
2020-07-27 10:58:54 -0400

    have dynamic info appending to dashboard

---
+++ public/js/randomfacts.js
@@ -0,0 +1,53 @@
+const randomfacts = {
+  rfacts: [
+    {
+      fact: "astrobiology is the official science and study of extraterresstrial life"
+    },
+    {
+      fact: "The earliest recordered UFO sighting was in 1440 B.C."
+    }, {
+      fact: "People believed in alien life as early as 610B.C"
+    }, {
+      fact: "62 out of 1,356 astronomers reported inexplicable ariel phenomena"
+    }, {
+      fact: "Alien sightings have been reported in many nuclear power plants"
+    }
+  ],
+  rimages: [
+    {
+      img: "https://images.unsplash.com/photo-1576527935473-1b8d6bfad277?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"
+    },
+    {
+      img: "https://media.gettyimages.com/illustrations/alien-artwork-illustration-id460713801?s=2048x2048"
+    }, {
+      img: "https://media.gettyimages.com/illustrations/illustration-of-a-humanoid-alien-illustration-id1124671867?s=2048x2048"
+    }, {
+      img: "https://www.history.com/.image/t_share/MTU3ODc3NjU1Mzk4ODUxOTEz/ufo-sightings-new-jersey-ufo-photos.jpg"
+    }, {
+      img: "https://media12.s-nbcnews.com/j/MSNBC/Components/Video/201909/ufothumb.focal-760x428.jpg"
+    }
+  ],
+  rquotes: [
+    {
+      quote: "The only thing that scares me more than space aliens is the idea that there aren't any space aliens. We can't be the best that creation has to offer. I pray we're not all there is. If so, we're in big trouble. ― Ellen DeGeneres"
+    },
+    {
+      quote: "We all know interspecies romance is weird. ― Tim Burton"
+    }, {
+      quote: "Man has gone out to explore other worlds and other civilizations without having explored his own labyrinth of dark passages and secret chambers, and without finding what lies behind doorways that he himself has sealed. ― Stanisław Lem, Solaris"
+    }, {
+      quote: "As human beings, we're born believing that we are the apex of creation, that we are invincible, that no problem exists that we cannot solve. But we inevitably die with all our beliefs broken. ― Shaun David Hutchinson, We Are the Ants"
+    }, {
+      quote: "It is surely unreasonable to credit that only one small star in the immensity of the universe is capable of developing and supporting intelligent life. But we shall not get to them and they will not come to us. ― P.D. James, The Children of Men"
+    }
+  ]
+
+};
+
+module.exports = randomfacts;
+
+
+
+
+
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 const path = require("path");
 const auth = require("../middleware/auth");
+const randomfacts = require("../public/js/randomfacts");
 
 
 // user authorized views - they all use the "auth" middleware
-router.get("/", auth, (req, res) => res.render("dashboard"));
+router.get("/", auth, (req, res) => {
+  const randomFact = randomfacts.rfacts[Math.floor(Math.random() * randomfacts.rfacts.length)];
+  const randomImage = randomfacts.rimages[Math.floor(Math.random() * randomfacts.rimages.length)];
+  console.log(randomImage.img);
+  const randomQuote = randomfacts.rquotes[Math.floor(Math.random() * randomfacts.rquotes.length)];
+  res.render("dashboard", {
+    fact:randomFact.fact,
+    img:randomImage.img,
+    quote:randomQuote.quote
+  });
+});
 router.get("/sightings", auth, (req, res) => res.render("sightings"));
 // router.get("/user/profile", auth, (req, res) => res.sendFile(path.join(__dirname, "../public/profile.html")));
 
