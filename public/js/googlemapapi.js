f6d9d448b7067933198c2dfeb24fae79e6b6110f
Cameron Graham <cameron.g882@gmail.com>
2020-07-28 10:43:28 -0400

    fixed up some styling on the dashboard page

--- public/js/googlemapapi.js
+++ public/js/googlemapapi.js
@@ -1,15 +1,18 @@
 /* eslint-disable linebreak-style */
 "use strict";
+// declare some constants for maps information
 var locations = [];
 let map;
 var alien = "/images/aliensmaller.png";
 var marker, i;
 
+// function to load the initial map containing any sightings since 2014
 function startPage() {
   $.ajax({
     url: "/api/sightings",
     method: "GET"
   }).then(function (data) {
+    // loop to determine the year of the data and put data since 2014 into the location array
     var length = data.length;
     for (let g = 0; g < length; g++) {
       var date = parseInt(data[g].datetime);
@@ -20,6 +23,7 @@ function startPage() {
       }
     } return locations;
   }).then(function (locations) {
+    // call the map api element and center it on the us
     map = new google.maps.Map(document.getElementById("map"), {
       center: {
         lat: 39.1299577,
@@ -366,6 +370,7 @@ function startPage() {
         }
       ]
     });
+    // loop to input the locations array information into markers on the map
     for (let i = 0; i < locations.length; i++) {
       var marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][0], locations[i][1]),
@@ -373,7 +378,9 @@ function startPage() {
         icon: alien,
         map: map,
         description: locations[i][3],
-      }); google.maps.event.addListener(marker, 'click', (function (marker, i) {
+      }); 
+      // listener to pull up information about the sighting on marker click and close any othe infowindows open
+      google.maps.event.addListener(marker, 'click', (function (marker, i) {
         return function () {
           infowindow.close();
           infowindow.setContent(`<div class= "infobox"> Siting location: ${locations[i][0]} <br><br>
@@ -385,7 +392,7 @@ function startPage() {
   });
 }
 
-
+// similar function to handle the state specific search
 function stateSearch() {
   locations = [];
   var selected = $("#exampleSelect2").find('option:selected');
@@ -767,6 +774,7 @@ function stateSearch() {
     } var infowindow = new google.maps.InfoWindow;
   });
 }
+// click function to initiate state search
 $("#searchBtn").on("click", function () {
   stateSearch();
 });
