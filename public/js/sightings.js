ff85cb97cd129c833fec1a4d023a89d5831a60fe
Cameron Graham <cameron.g882@gmail.com>
2020-07-31 04:42:20 -0400

    switched map year to 2003

--- public/js/sightings.js
+++ public/js/sightings.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable linebreak-style */
-$("#add").on("click", function(event){
+$("#add").on("click", function (event) {
 
   event.preventDefault();
 
@@ -12,22 +12,26 @@ $("#add").on("click", function(event){
     duration_hours_min: $("#duration_hours_min").val().trim(),
     comments: $("#comments").val().trim(),
     latitude: 39.1299577,
-    longitude: -96.44058   
+    longitude: -96.44058
   };
   console.log(newSighting);
-  $.ajax("/sightings",{
+  createSighting(newSighting);
+});
+
+function createSighting(newSighting) {
+  $.ajax("/sightings", {
     type: "POST",
     data: newSighting
   })
-    .then(function(){
+    .then(function () {
       console.log("Your sighting is added");
       location.reload();
     });
-    
+
   // empty each input box by replacing the value with an empty string
   $("#datetime").val("");
   $("#state").val("");
   $("#shape").val("");
   $("#duration_hours_min").val("");
   $("#comments").val("");
-});
+}
